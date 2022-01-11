65ee077df35fe99a6f76c5c0cd08671bd65dc9ef
Ilya Nixan <thenixan@gmail.com>
2022-01-11 12:08:40 +0300

    Test case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TestServiceService} from "./test-service.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+    isLoading = false;
+    result = 0;
+
+    constructor(
+        private service: TestServiceService,
+    ) {
+    }
+
+    test() {
+        this.isLoading = true;
+        this.service.someHeavyJob()
+            .subscribe(
+                (c) => {
+                    this.isLoading = false;
+                },
+                (e) => {
+                    this.isLoading = false;
+                    console.log(e);
+                },
+            );
+        this.result = this.service.counter;
+    }
 }
